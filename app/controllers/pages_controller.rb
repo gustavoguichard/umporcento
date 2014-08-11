65b05643422445ec0d7bf335f5f30817de1b7cd2
Gustavo Guichard <gustavoguichard@gmail.com>
2014-08-11 00:30:56 -0300

    Adjusted social menu order and Home Slider slides and order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,10 @@ class PagesController < ApplicationController
 
   def home
     @slides = [
-      {url: "slides/slide-1.jpg", caption_1: "Quando foi a", caption_2: "última vez...", title: "Social"},
-      {url: "slides/slide-2.jpg", caption_1: "Que você abraçou", caption_2: "alguém de verdade?", title: "Interpessoal"},
       {url: "slides/slide-3.jpg", caption_1: "Que você andou", caption_2: "de bicicleta?", title: "Pessoal"},
-      {url: "slides/slide-4.jpg", caption_1: "Que você brincou", caption_2: "com seus filhos?", title: "Familiar"}
+      {url: "slides/slide-2.jpg", caption_1: "Que você abraçou", caption_2: "alguém de verdade?", title: "Interpessoal"},
+      {url: "slides/slide-4.jpg", caption_1: "Que você brincou", caption_2: "com seus filhos?", title: "Familiar"},
+      {url: "slides/slide-1.jpg", caption_1: "Que você se dedicou", caption_2: "a uma causa nobre?", title: "Social"}
     ]
   end
 
